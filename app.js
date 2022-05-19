bc8407387bd43c7e5d7ad71ce237ead6a00066e6
Mostafa El masry <33985803+sasaku2020@users.noreply.github.com>
2022-05-19 14:15:04 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,18 @@
+const target = document.querySelector('.listing ul')
+
+const buttons = document.querySelectorAll('.btn-1');
+for (let button of buttons) {
+    this.addEventListener('click', function() {
+
+        const addtext = document.querySelector(".addtext");
+        if (addtext.value === "") {
+
+        } else {
+            const li = document.createElement('li');
+            li.innerHTML = addtext.value
+            target.appendChild(li);
+            addtext.value = "";
+        }
+
+    })
+}
